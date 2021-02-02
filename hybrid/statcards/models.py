0abb18808b83a136db543665055a6132aef0e61c
Maksim Maksimov <maksdev@bk.ru>
2021-02-02 09:53:45 +0300

    init 20210202_0001

--- hybrid/statcards/models.py
+++ hybrid/statcards/models.py
@@ -5,8 +5,8 @@ class SPR_F10R1(models.Model):
     code = models.CharField(max_length=2, unique=True, verbose_name='Код')
     name = models.CharField(max_length=100, verbose_name='Наименование')
     class Meta:
-        verbose_name = 'F1.0 R01 Орган'
         verbose_name_plural = 'F1.0 R01 Органы'
+        verbose_name = 'F1.0 R01 Орган'
     def __str__(self):
         return self.code + ' | ' + self.name
 
@@ -63,3 +63,146 @@ class SPR_F11R25(models.Model):
         verbose_name_plural = 'F1.1 R25 Решения по преступлениям'
     def __str__(self):
         return self.code + ' | ' + self.name
+
+# class SPR_F10R7_1(models.Model):
+#     code = models.CharField(max_length=1, unique=True, verbose_name='Код')
+#     name = models.CharField(max_length=100, verbose_name='Наименование')
+#     class Meta:
+#         verbose_name = 'F1.0 R7.1 Восстановлено на учет в числе ППЛ'
+#         verbose_name_plural = 'F1.0 R7.1 Восстановлены на учет в числе ППЛ'
+#     def __str__(self):
+#         return self.code + ' | ' + self.name
+#
+# #r10.3=#r9=8112
+# class SPR_F10R9(models.Model):
+#     code = models.CharField(max_length=5, unique=True, verbose_name='Код')
+#     name = models.CharField(max_length=100, verbose_name='Наименование')
+#     class Meta:
+#         verbose_name = 'F1.0 R9 Преступление выявлено'
+#         verbose_name_plural = 'F1.0 R9 Преступления выявлены'
+#     def __str__(self):
+#         return self.code + ' | ' + self.name
+#
+# class SPR_F10R9_1(models.Model):
+#     code = models.CharField(max_length=1, unique=True, verbose_name='Код')
+#     name = models.CharField(max_length=100, verbose_name='Наименование')
+#     class Meta:
+#         verbose_name = 'F1.0 R9.1'
+#         verbose_name_plural = 'F1.0 R9.1'
+#     def __str__(self):
+#         return self.code + ' | ' + self.name
+#
+# class SPR_F10R10(models.Model):
+#     code = models.CharField(max_length=4, unique=True, verbose_name='Код')
+#     name = models.CharField(max_length=100, verbose_name='Наименование')
+#     class Meta:
+#         verbose_name = 'F1.0 R10'
+#         verbose_name_plural = 'F1.0 R10'
+#     def __str__(self):
+#         return self.code + ' | ' + self.name
+#
+# class SPR_F10R10_1(models.Model):
+#     code = models.CharField(max_length=4, unique=True, verbose_name='Код')
+#     name = models.CharField(max_length=100, verbose_name='Наименование')
+#     class Meta:
+#         verbose_name = 'F1.0 R10.1'
+#         verbose_name_plural = 'F1.0 R10.1'
+#     def __str__(self):
+#         return self.code + ' | ' + self.name
+#
+# class SPR_F10R10_2(models.Model):
+#     code = models.CharField(max_length=3, unique=True, verbose_name='Код')
+#     name = models.CharField(max_length=100, verbose_name='Наименование')
+#     class Meta:
+#         verbose_name = 'F1.0 R10.2'
+#         verbose_name_plural = 'F1.0 R10.2'
+#     def __str__(self):
+#         return self.code + ' | ' + self.name
+#
+# class SPR_F10R11(models.Model):
+#     code = models.CharField(max_length=1, unique=True, verbose_name='Код')
+#     name = models.CharField(max_length=100, verbose_name='Наименование')
+#     class Meta:
+#         verbose_name = 'F1.0 R11'
+#         verbose_name_plural = 'F1.0 R11'
+#     def __str__(self):
+#         return self.code + ' | ' + self.name
+#
+# class SPR_F10R13(models.Model):
+#     code = models.CharField(max_length=5, unique=True, verbose_name='Код')
+#     name = models.CharField(max_length=100, verbose_name='Наименование')
+#     class Meta:
+#         verbose_name = 'F1.0 R13 Квалификация'
+#         verbose_name_plural = 'F1.0 R13 Квалификации'
+#     def __str__(self):
+#         return self.code + ' | ' + self.name
+
+#r13-551
+#15-8104
+#r16-8105
+#17-8106
+#r18-8108
+#r19_1-8210
+#r19_2-8211
+#r19_3-8107
+#r20-525
+#r21-8109
+#r21.1-8110
+#r22-503
+#r23-8196
+#r24-8111
+#r25k-8241
+#r25.1k=8219
+#r26-526
+#r27-528
+#r28-8113
+#29-8205
+#29.1-8114
+#r32-8115
+#r33-8116
+#r33-8192
+#r36-516
+#r37-516
+#r37-8117
+#r40-8118
+
+#форма 1.1
+#r9-8119
+#r10-8120
+#r11-8121
+#r12-8122
+#13-8123
+#14-8124
+#15-8125
+#16-8126
+#17-8127
+#18-516
+#18.1-516
+#19-516
+#20-8128
+#21-8129
+#22-8128
+#23-8192
+#26-565
+#27=8130
+#28k-8131
+#30-8132
+#31-8133
+#32-8134
+#32.1-8139
+#33-552
+#34-8112
+#35-8193-8194-8195
+#35.1-8193
+#36-8135
+
+#форма 1.2
+
+#r9-8136
+#r10-255
+#r11-8137
+#r13-8138
+#r14-8112
+#14.1-8217
+#r14.2-8135
+
