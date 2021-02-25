55e541c66ad9ad824de2c0d590c85640a53ff6f3
Maksim Maksimov <maksdev@bk.ru>
2021-02-25 09:21:54 +0300

    init 20210225 0001

--- hybrid/statcards/admin.py
+++ hybrid/statcards/admin.py
@@ -2,27 +2,7 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(SPR_F10R1)
-admin.site.register(SPR_F10R1_ORGAN)
-admin.site.register(SPR_F10R2)
-admin.site.register(SPR_F10R3)
-admin.site.register(SPR_F10R3_GASORG)
-admin.site.register(SPR_F10R7_1)
-admin.site.register(SPR_F10R8)
-admin.site.register(SPR_F10R9)
-admin.site.register(SPR_F10R9_1)
-admin.site.register(SPR_F10R10)
-admin.site.register(SPR_F10R10_1)
-admin.site.register(SPR_F10R10_2)
-admin.site.register(SPR_F10R11)
-admin.site.register(SPR_F10R13)
-admin.site.register(SPR_F10R15)
-admin.site.register(SPR_F10R16)
-admin.site.register(SPR_F10R17)
-admin.site.register(SPR_F10R18)
-admin.site.register(SPR_F10R19_1)
-admin.site.register(SPR_F10R19_2)
-admin.site.register(SPR_F10R19_3)
+
 
 
 # @admin.register(Post)
--- hybrid/statcards/apps.py
+++ hybrid/statcards/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class StatcardsConfig(AppConfig):
     name = 'statcards'
+    verbose_name = 'Статистические карты'
